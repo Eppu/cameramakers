c4092751eadf551e701435688c895d3d06842bac
Misa Jokisalo <misa.jokisalo@gmail.com>
2018-03-06 16:17:11 +0200

    Improved validation user experience and documentation

--- src/ContactForm/index.js
+++ src/ContactForm/index.js
@@ -14,22 +14,61 @@ const errorMessages = {
 
 
 class ContactForm extends React.Component {
-
+  /**
+   * Validates input values.
+   * Returns an error message if an input is invalid or undefined if an input is valid.
+   * @param {string} name Input field's name
+   * @param {object} item Input's corresponding object in state.
+   * @returns {(string|undefined)} Error message, or undefined if there is no error.
+   */
   static isValid(name, item) {
-    // Validate a single input
+    // Input cannot be empty
     if (!item.value.length > 0) {
-      // Input is empty
       return errorMessages.emptyInput;
     }
 
-    console.log(name);
-
-    // Check if email is valid
+    // Email input has to be a valid email address
     const mailFormat = /\S+@\S+\.\S/;
     if (name === 'email' && !item.value.match(mailFormat)) return errorMessages.invalidEmail;
 
-    // Everything is valid
+    // If everything is valid...
     return undefined;
+  }
+
+
+  /**
+   * Tries to make an input VALID by removing its error message.
+   * Call this whenever an input's value changes. We want to show the user that
+   * their input is correct as soon as possible.
+   * @param {string} name Input field's name
+   * @param {object} item Input's corresponding object in state.
+   * @returns {object} Updated input data object to be saved in state.
+   */
+  static tryToMakeValid(name, item) {
+    const error = ContactForm.isValid(name, item);
+    if (error === undefined) {
+      // Input is valid. Remove error.
+      return { ...item, error: undefined };
+    }
+    return item;
+  }
+
+
+  /**
+   * Tries to make an input INVALID by giving it an error message.
+   * Call this whenever an input is blurred. We don't want to warn the user when
+   * they are still typing.
+   * @param {string} name Input field's name
+   * @param {object} item Input's corresponding object in state.
+   * @returns {object} Updated input data object to be saved in state.
+   */
+  static tryToMakeInvalid(name, item) {
+    const error = ContactForm.isValid(name, item);
+    if (error !== undefined) {
+      // Input is invalid. Save the error
+      return { ...item, error };
+    }
+    return item;
   }
 
 
@@ -39,54 +78,79 @@ class ContactForm extends React.Component {
       firstName: {
         value: '',
         touched: false,
-        isValid: false,
         error: errorMessages.emptyInput,
       },
       lastName: {
         value: '',
         touched: false,
-        isValid: false,
         error: errorMessages.emptyInput,
       },
       email: {
         value: '',
         touched: false,
-        isValid: false,
         error: errorMessages.emptyInput,
       },
       subject: {
         value: '',
         touched: false,
-        isValid: false,
         error: errorMessages.emptyInput,
       },
       message: {
         value: '',
         touched: false,
-        isValid: false,
         error: errorMessages.emptyInput,
       },
     };
+    this.handleBlur = this.handleBlur.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.formIsValid = this.formIsValid.bind(this);
     this.sendMail = this.sendMail.bind(this);
   }
 
 
+  /**
+   * Handle changes in inputs. Tries to validate inputs and updates state.
+   * @param {object} event Change event that was fired
+   */
   handleChange(event) {
-    // Update value and touched state
+    // Get name and value from the event.
     const { name, value } = event.target;
-    let newItem = { ...this.state[name], value, touched: true };
 
-    // Validate value to get a potential error. Add error to object
-    const error = ContactForm.isValid(name, newItem);
-    newItem = { ...newItem, error };
+    // Update value
+    let newItem = { ...this.state[name], value };
+
+    // Validate input, trying to remove any error messages that were present.
+    newItem = ContactForm.tryToMakeValid(name, newItem);
 
     // Update state
     this.setState({ [name]: newItem });
   }
 
 
+  /**
+   * Handle blurring of inputs. Tries to invalidate inputs and updates validation errors into state.
+   * @param {object} event Blur event that was fired
+   */
+  handleBlur(event) {
+    // Get name from the event, value from state.
+    const { name } = event.target;
+    let newItem = this.state[name];
+
+    // Validate input's value. An error message is added if the input is invalid.
+    newItem = ContactForm.tryToMakeInvalid(name, newItem);
+
+    // Mark that this item was touched
+    newItem = { ...newItem, touched: true };
+
+    // Update state
+    this.setState({ [name]: newItem });
+  }
+
+
+  /**
+   * Checks if entire form is valid.
+   * @returns {bool} Whether or not the form is valid.
+  */
   formIsValid() {
     const errors = Object.values(this.state).filter(item => item.error !== undefined);
     // If errors exist, form isn't valid.
@@ -95,6 +159,9 @@ class ContactForm extends React.Component {
   }
 
 
+  /**
+   * Sends form data to email service.
+   */
   sendMail() {
     console.log(this.state);
   }
@@ -112,6 +179,7 @@ class ContactForm extends React.Component {
               floatingLabelText="First Name"
               name="firstName"
               onChange={this.handleChange}
+              onBlur={this.handleBlur}
               errorText={this.state.firstName.touched ? this.state.firstName.error : ''}
               fullWidth
             />
@@ -123,6 +191,7 @@ class ContactForm extends React.Component {
               floatingLabelText="Last Name"
               name="lastName"
               onChange={this.handleChange}
+              onBlur={this.handleBlur}
               errorText={this.state.lastName.touched ? this.state.lastName.error : ''}
               fullWidth
             />
@@ -133,6 +202,7 @@ class ContactForm extends React.Component {
             floatingLabelText="Email Address"
             name="email"
             onChange={this.handleChange}
+            onBlur={this.handleBlur}
             type="email"
             errorText={this.state.email.touched ? this.state.email.error : ''}
             fullWidth
@@ -143,6 +213,7 @@ class ContactForm extends React.Component {
             floatingLabelText="Subject"
             name="subject"
             onChange={this.handleChange}
+            onBlur={this.handleBlur}
             errorText={this.state.subject.touched ? this.state.subject.error : ''}
             fullWidth
           />
@@ -152,6 +223,7 @@ class ContactForm extends React.Component {
             floatingLabelText="Message"
             name="message"
             onChange={this.handleChange}
+            onBlur={this.handleBlur}
             errorText={this.state.message.touched ? this.state.message.error : ''}
             fullWidth
             multiLine
